8b7a52a1f09df7133a6f243b1c5e07dd9055dbdc
Alejo (desde Windows) <alejom.tv@gmail.com>
2022-10-05 22:12:53 -0500

    Vectors are drawn

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,33 @@ using namespace std;
 using namespace cv;
 
 #define EPSILON 1e-10
+#define PI 3.1416
 #define FEATURE_VECTOR_SIZE 1764 // = 7 * 7 * 36
 #define EULER 2.71828
 #define EPOCHS 100
 #define WEIGHTS_FILE_PATH "weights.data"
-
+const float cosine[9] = {
+    1,
+    0.939692,
+    0.766043,
+    0.499998,
+    0.173645,
+    -0.173652,
+    -0.500004,
+    -0.766048,
+    -0.939695
+};
+const float sine[9] = {
+    0,
+    0.342021,
+    0.642789,
+    0.866027,
+    0.984808,
+    0.984807,
+    0.866023,
+    0.642783,
+    0.342014,
+};
 
 vector<string> getImages(const char* dirname)
 {
@@ -78,6 +100,11 @@ int getFeatureVector(string path, float* featureVector) {
         cerr << "Could not read the image: " << path << endl;
         return 1;
     }
+
+    // Resize image
+    resize(img, resizedImg, Size(896, 896));
+    imshow("original", resizedImg);
+    waitKey(0);
 
     // Resize image
     resize(img, resizedImg, Size(64, 64));
@@ -163,6 +190,31 @@ float predict(float* featureVector, float* weights) {
     return 1 / (1 + pow(EULER, -sum));
 }
 
+void shuffle(unsigned char* labels, float** examples, size_t n)
+{
+    if (n > 1)
+    {
+        size_t i;
+        for (i = 0; i < n - 1; i++)
+        {
+            size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+            float* t;
+            t = examples[j];
+            examples[j] = examples[i];
+            examples[i] = t;
+            unsigned char w = labels[j];
+            labels[j] = labels[i];
+            labels[i] = w;
+        }
+    }
+}
+
+float cost(unsigned char label, float prediction) {
+    float cost1 = label * log(prediction);
+    float cost2 = (1 - label) * log(1 - prediction);
+    return -cost1 - cost2;
+}
+
 void trainLogRegression(unsigned int epochs, unsigned int examples, float** features,
     unsigned char* labels, float* weights) {
     unsigned int i, j, k;
@@ -173,16 +225,23 @@ void trainLogRegression(unsigned int epochs, unsigned int examples, float** feat
     }
 
     for (i = 0; i < epochs; i++) {
+        shuffle(labels, features, examples);
+        if (!(i % 100)) {
+            printf("Epoch %d/%d...\n", i, epochs);
+        }
+
+        float costo = 0;
         for (j = 0; j < examples; j++) {
             prediction = predict(features[j], weights);
             error = labels[j] - prediction;
-            weights[0] = weights[0] + alpha * error * prediction
-                * (1 - prediction);
+            // cout << "Error=" << error << endl;
+            weights[0] = weights[0] + alpha * error;
             for (k = 0; k < FEATURE_VECTOR_SIZE; k++) {
-                weights[k + 1] = weights[k + 1] + alpha
-                    * error * prediction * (1 - prediction) * features[j][k];
+                weights[k + 1] = weights[k + 1] + alpha * error * features[j][k];
             }
+            costo += cost(labels[j], prediction);
         }
+        cout << "Costo=" << costo / examples << endl;
     }
 }
 
@@ -215,6 +274,55 @@ unsigned char loadWeights(float* weights) {
     return 0;
 }
 
+void drawFeatureVector(float* featureVector) {
+    unsigned int i, j, k, x, y;
+    const int width = 64;
+    Mat img, img1, img2;
+
+    for (i = 0; i < 14; i++){
+        for (j = 0; j < 14; j++) {
+            if (!j){
+                img = Mat::zeros(width, width, CV_32F);
+                for (k = 0; k < 9; k++) {
+                    x = (unsigned int)(featureVector[i * 126 + j * 9 + k] * cosine[k] * width + width / 2);
+                    y = (unsigned int)(featureVector[i * 126 + j * 9 + k] * sine[k] * width + width / 2);
+                    line(
+                        img, 
+                        Point( width / 2, width / 2 ),
+                        Point( x, y ),
+                        Scalar(255),
+                        1,
+                        LINE_8
+                    );
+                }
+            } else {
+                img1 = Mat::zeros(width, width, CV_32F);
+                for (k = 0; k < 9; k++) {
+                    x = (unsigned int)(featureVector[i * 126 + j * 9 + k] * cosine[k] * width + width / 2);
+                    y = (unsigned int)(featureVector[i * 126 + j * 9 + k] * sine[k] * width + width / 2);
+                    line(
+                        img1, 
+                        Point( width / 2, width / 2 ),
+                        Point( x, y ),
+                        Scalar(255),
+                        1,
+                        LINE_8
+                    );
+                }
+                hconcat(img, img1, img);
+            }
+        }
+        if (img2.empty()) {
+            img2 = img.clone();
+        } else {
+            vconcat(img2, img, img2);
+        }
+    }
+
+    imshow("Display window", img2);
+    waitKey(0); // Wait for a keystroke in the window
+}
+
 int main(int argc, const char** argv)
 {
     const char* nonTiresPath;
@@ -235,7 +343,7 @@ int main(int argc, const char** argv)
         tiresPath = argv[1];
         nonTiresPath = argv[2];
         epochs = EPOCHS;
-        if (argc < 3) {
+        if (argc > 3) {
             epochs = atoi(argv[3]);
         }
         tireImagePaths = getImages(tiresPath);
@@ -259,7 +367,7 @@ int main(int argc, const char** argv)
             getFeatureVector(string(nonTiresPath) + noTireImagePaths[i],
                 featureVector);
             features[i + tireImagesVectorSize] = featureVector;
-            labels[i] = 0;
+            labels[i + tireImagesVectorSize] = 0;
         }
 
         trainLogRegression(epochs, tireImagesVectorSize + noTireImagesVectorSize,
@@ -293,7 +401,7 @@ int main(int argc, const char** argv)
             return -1;
         }
         getFeatureVector(string(argv[1]), featureVector);
-
+        drawFeatureVector(featureVector);
         prediction = predict(featureVector, weights);
 
         if (prediction > 0.5) {
@@ -314,3 +422,4 @@ int main(int argc, const char** argv)
     return -1;
 
 }
+
